9ef7bf5a7a970a0c3f34929f97695c5cbd22d87e
Ronald <ronaldmoralesm@gmail.com>
2019-06-22 19:07:45 -0400

    agrego health

--- src/Status.gd
+++ src/Status.gd
@@ -3,10 +3,20 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+export var _health:int = 0 
+#export var _isDead:bool = setget set_isDead, get_isDead
+#export var _isAlive:bool = setget set_isAlive, get_isAlive
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func set_health(health: int) -> void:
+	_health = health
+
+func get_health() -> int:
+	return _health
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
