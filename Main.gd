4dbad1fc97f7c4ed13d976be20483b403ed25874
Ronald <ronaldmoralesm@gmail.com>
2019-06-23 16:18:22 -0400

    formateo

--- Main.gd
+++ Main.gd
@@ -1,10 +1,7 @@
 extends Node
 
-#var test = preload("res://src/body.gd")
 var Player = preload("res://src/Player.tscn")
-var Body = preload("res://src/body.gd")
 
 func _ready():
 	var player = Player.instance()
 	add_child(player)
-
--- Player.gd
+++ Player.gd
@@ -1,16 +1,11 @@
 class_name Player extends Node
 
 var Body = preload("res://src/body.gd")
-
-export var id:int = 0
-export var nickname:String = 'Default'
 var body:KinematicBody2D =  Body.new()
-
-
 var Status = preload("res://src/status.gd")
 var body_class = preload("res://src/body.gd")
-
-
+export var id:int = 0
+export var nickname:String = 'Default'
 export var status:GDScript = Status.new()
 
 func _init():
