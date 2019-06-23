2a01e854bb6ca819368c4b852ddb1174564d1e58
Peter <pedrocisternasd@gmail.com>
2019-06-23 15:27:28 -0400

    programando terrible de volaoo estoYYY vOOLALLALAOPOOOAOAOAO uuuA AOOAAO OAOA!!!

--- Main.gd
+++ Main.gd
@@ -6,4 +6,11 @@ var player_class = preload("res://src/Player.tscn")
 func _ready():
 	var player = player_class.instance()
 	add_child(player)
-
+	print(player.nickname)
+	player.nickname = "caca"
+	print(player.nickname)
+	print(player.status.health)
+	
+	player.status.health = 666
+	
+	print(player.status.health)
--- Player.gd
+++ Player.gd
@@ -2,7 +2,8 @@ class_name Player extends Node
 
 export var id:int = 0
 export var nickname:String = 'Default'
-export var Status = preload("res://src/status.gd")
+
+var Status = preload("res://src/status.gd")
 var body_class = preload("res://src/body.gd")
 
 
@@ -11,12 +12,3 @@ export var status:GDScript = Status.new()
 func _init():
 	var body = body_class.new()
 	add_child(body)
-
-func _ready():
-	pass
-
-func set_nickname(_nickname: String) -> void:
-	nickname = _nickname
-
-func get_nickname() -> String:
-	return nickname
