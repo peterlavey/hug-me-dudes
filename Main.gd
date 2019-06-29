82950562b120eed03afed7ed4f5cf4e2d45171e0
Peter <pedrocisternasd@gmail.com>
2019-06-29 16:38:18 -0400

    creacion e instanciacion de timer

--- Main.gd
+++ Main.gd
@@ -2,11 +2,13 @@ extends Node
 
 var Player = load("res://src/player.gd")
 var Stage = preload("res://stages/Stage.tscn")
+var Disease = load("res://src/disease.gd")
 
 func _ready():
 	var player1 = Player.new()
 	var player2 = Player.new()
 	var stage = Stage.instance()
+	var disease = Disease.new()
 	
 	player1._id = 1
 	player1.texture = load("res://sprites/icon.png")
@@ -14,11 +16,17 @@ func _ready():
 	player1.position.y = 0
 	
 	player2._id = 2
-	player2.texture = load("res://sprites/icon2.png")
+	player2.texture = load("res://sprites/icon.png")
 	player2.position.x = 600
 	player2.position.y = 0
+	
+	disease.position.x = 450
+	disease.position.y = 50
 	
 	add_child(stage)
 	add_child(player1)
 	add_child(player2)
+	add_child(disease)
+	
+	
 
---
+++ src/disease.gd
@@ -0,0 +1,48 @@
+class_name Diseases extends Node2D
+
+var input:TextEdit
+var timer:Timer
+var timeLeft:float
+
+func _init():
+	config_timer()
+	config_input()
+	
+	pass
+
+func _process(delta) -> void:
+	show_time_left()
+	
+	pass
+	
+func set_desease() -> void:
+	print("TIMER")
+	
+	pass
+
+func show_time_left() -> void:
+	timeLeft = timer.get_time_left()
+	input.set_text(str(stepify(timeLeft, 0.01)))
+	
+	pass
+
+func config_input() -> void:
+	input = TextEdit.new()
+	input.rect_size.x = 100
+	input.rect_size.y = 20
+	
+	add_child(input)
+	
+	pass
+
+func config_timer() -> void:
+	timer = Timer.new()
+	#timer.set_autostart(true)
+	timer.set_one_shot(true)
+	timer.set_wait_time(3)
+	timer.connect("timeout", self, "set_desease")
+	timer.start()
+	
+	add_child(timer)
+	
+	pass
