a540a8609e5e6e052d38f25880343de514c2e860
Peter <pedrocisternasd@gmail.com>
2019-07-08 00:07:48 -0400

    IOC en playlist

--- src/playlist/musicPlayer.gd
+++ src/playlist/musicPlayer.gd
@@ -4,9 +4,11 @@ export var songs: Array
 export var shuffle: bool = true
 export var currentSong: String
 var currentIndex:int = 0
+var input
 
 func _init():
 	#todo: no funciona el emit
+	config_input()
 	connect("finished", self, "next_song")
 	pass
 
@@ -21,14 +23,33 @@ func next_song()-> void:
 	pass
 
 func start():
-	var song = load('res://sounds/ost/' + songs[currentIndex])
+	set_current_song()
+	load_song()
+	play(stream.get_length())
+	verify_list()
+
+func set_current_song():
+	currentSong = songs[currentIndex]
+	input.set_text(currentSong.split(".")[0])
+
+func load_song():
+	var song = load('res://sounds/ost/' + currentSong)
 	stream = song
-	play()
-	
+
+func verify_list():
 	if currentIndex < songs.size() -1:
 		currentIndex += 1
 	else:
 		currentIndex = 0
+
+func config_input() -> void:
+	input = TextEdit.new()
+	input.rect_size.x = 500
+	input.rect_size.y = 30
+	
+	add_child(input)
+	
+	pass
 
 func _process(delta):
 	if get_playback_position() > stream.get_length() -0.05:
